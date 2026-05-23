5dbcc418a1c9f52de762030e5e7e85e2091ba1c6
dk <kudin.dima@gmail.com>
2026-05-23 16:18:42 +0000

    Initial commit

---
+++ src/app/layout.tsx
@@ -0,0 +1,47 @@
+import type { Metadata } from "next";
+import { Halant } from "next/font/google";
+import { Inter } from "next/font/google";
+import "./globals.css";
+import "@/lib/gsap-setup";
+import { ServiceWrapper } from "@/components/ServiceWrapper";
+import Tag from "@/tag/Tag";
+import { getVisualEditScript } from "@/utils/visual-edit-script";
+import { Roboto } from "next/font/google";
+
+
+
+export const metadata: Metadata = {
+  title: 'وسيط عقاري - يونس أصيلة | أفضل العقارات في أصيلة',
+  description: 'يونس أصيلة يوفر لكم أفضل عروض البيع والكراء في مدينة أصيلة. عقارات موثقة وخدمة احترافية.',
+  openGraph: {
+    "siteName": "يونس أصيلة"
+  },
+};
+
+const roboto = Roboto({
+  variable: "--font-roboto",
+  subsets: ["latin"],
+  weight: ["100", "300", "400", "500", "700", "900"],
+});
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en" suppressHydrationWarning>
+      <ServiceWrapper>
+        <body className={`${roboto.variable} antialiased`}>
+          <Tag />
+          {children}
+          <script
+              dangerouslySetInnerHTML={{
+                  __html: `${getVisualEditScript()}`
+              }}
+          />
+        </body>
+      </ServiceWrapper>
+    </html>
+  );
+}
